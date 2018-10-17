72d45d70e5a3367fda2b4ea223995d2a0ef2a3d8
Christian Dupuis <cd@atomist.com>
2018-10-17 21:24:54 +0200

    Initial commit from Atomist

---
+++ src/main/java/com/atomist/Cd49Application.java
@@ -0,0 +1,12 @@
+package com.atomist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cd49Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cd49Application.class, args);
+	}
+}
