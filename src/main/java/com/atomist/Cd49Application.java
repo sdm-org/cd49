8c4317d04a71bcb991e8eef268271db361fef0b0
Christian Dupuis <cd@atomist.com>
2018-12-14 00:33:15 +0100

    Update Cd49Application.java

--- src/main/java/com/atomist/Cd49Application.java
+++ src/main/java/com/atomist/Cd49Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Cd49Application {
-          
+           
 	public static void main(String[] args) {
 		SpringApplication.run(Cd49Application.class, args);
 	}
